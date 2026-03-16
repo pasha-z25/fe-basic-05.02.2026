f8649243f17c98e2db071bafb9bd215fd7d1c960
Pasha Zubak <pashazubak@gmail.com>
2026-03-16 21:15:53 +0200

    add header and hero to main page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('It works!');
+console.log("It works!");
+
+$("#burger").on("click", function () {
+  $(this).toggleClass("active");
+});
+
+// const burgerEl = document.getElementById("burger");
+// burgerEl.addEventListener('click',)
