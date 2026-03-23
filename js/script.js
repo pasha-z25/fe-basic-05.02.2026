b0bb86d3c2df70463582dc71d96de005bcc02b2c
Pasha Zubak <pashazubak@gmail.com>
2026-03-23 20:52:36 +0200

    add lesson 13

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('It works!');
+console.log("It works!");
+
+$("#loadBtn").on("click", function () {
+  $(".wrapper").addClass("active");
+});
+
+$(".wrapper").on("click", function () {
+  $(this).removeClass("active");
+});
