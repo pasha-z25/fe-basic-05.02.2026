9ebfce87239ff1e45313c45647acbdafd773d260
Pasha Zubak <pashazubak@gmail.com>
2026-04-06 22:14:39 +0300

    add new sections

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
-console.log("It works!");
+console.log('It works!');
 
-$("#burger").on("click", function () {
-  $(this).toggleClass("active");
+$('#burger').on('click', function () {
+  $(this).toggleClass('active');
+});
+
+$('#openModal').on('click', function () {
+  $('.modal-wrapper').addClass('active');
+});
+
+$('.modal-window .close, .modal-wrapper').on('click', function (event) {
+  if (event.target === this) {
+    $('.modal-wrapper').removeClass('active');
+  }
+});
+
+/**
+ * Автоматично оновлює поточний рік у сніданті
+ * Знаходить елемент з ID 'currentYear' і встановлює його вміст
+ * на поточний рік (отримується з Date.getFullYear())
+ */
+const currentYearEl = document.getElementById('currentYear');
+const currentYear = new Date().getFullYear();
+currentYearEl.textContent = currentYear;
+
+const submitBtn = document.getElementById('submitBtn');
+submitBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+  alert('Форма відправлена!');
 });
 
 // const burgerEl = document.getElementById("burger");
